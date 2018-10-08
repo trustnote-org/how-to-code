094850a5a7f3ec5e040353daad2e6120595b60a6
makeabignews <makeabignews@sina.com>
2018-10-08 03:41:04 -0400

    Create cli.py

---
+++ samples/miningDashboard/cli.py
@@ -0,0 +1,15 @@
+import miningHelper.py
+
+print ("pow:{0}".format(helper.get_pow()))
+print ("trustme:{0}".format(helper.get_trustme()))
+print ("coinbase:{0}".format(helper.get_coinbase()))
+
+print ("TTT:{0} MN".format(float(helper.get_coinbase())/1000000))
+
+print ("pow count:{0}".format(helper.get_pow_count()))
+print ("trustme count:{0}".format(helper.get_trustme_count()))
+print ("coinbase count :{0}".format(helper.get_coinbase_count()))
+
+round_index = helper.get_round_index()
+print ("round_index:{0}".format())
+print ("am in trustme?:{0}".format(helper.is_round_in(round_index-1)))
