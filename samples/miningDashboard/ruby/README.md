f69f352ef4b7836af206832e8043ae86661aa8e0
makeabignews <makeabignews@sina.com>
2018-10-11 06:06:12 -0400

    ruby

--- samples/miningDashboard/ruby/README.md
+++ samples/miningDashboard/ruby/README.md
@@ -1,4 +1,34 @@
+### install
+
 ```
 sudo apt-get install libsqlite3-dev
 sudo gem install sqlite3
+sudo gem install ramaze
 ```
+
+### run trustnote-pow-supernode
+
+```
+git clone https://github.com/trustnote/trustnote-pow-supernode.git
+cd trustnote-pow-supernode
+npm install
+node start.js
+```
+
+### run in Terminal
+
+```
+ruby cli.rb
+```
+
+### run in web
+
+```
+ruby web.rb
+```
+
+http://localhost:7000/
+
+### Screenshot
+
+![](Screenshot.png)
---
+++ samples/miningDashboard/ruby/web.rb
@@ -0,0 +1,49 @@
+require 'ramaze'
+require "./miningHelper.rb"
+
+
+class Home < Ramaze::Controller
+  @@helper = Helper.new
+  @@helper.init
+  map '/'
+  def index
+    # "Hello Dashboard"
+    render_file 'view/home.xhtml',:title => "Dashboard"
+  end
+end
+
+class Api < Ramaze::Controller
+    @@helper = Helper.new
+    @@helper.init
+    map '/api/'
+
+    def address
+        @@helper.get_address
+    end
+    def round
+        @@helper.get_round_index
+    end
+end
+
+class Api_Count < Ramaze::Controller
+    @@helper = Helper.new
+    @@helper.init
+    map '/api/count'
+    def unit
+      @@helper.get_unit_count
+    end
+    def pow
+      @@helper.get_pow_count
+    end
+    def trustme
+      @@helper.get_trustme_count
+    end
+    def coinbase
+        @@helper.get_coinbase_count
+    end
+    def ttt
+        @@helper.get_ttt/1000000
+    end
+end
+
+Ramaze.start
