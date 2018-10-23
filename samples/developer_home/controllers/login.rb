0c39c14434a8271d40351fa44b8930397bbc01fc
makeabignews <makeabignews@sina.com>
2018-10-23 06:04:15 -0400

    up

--- samples/developer_home/controllers/login.rb
+++ samples/developer_home/controllers/login.rb
@@ -1,5 +1,8 @@
+require "sqlite3"
+require "pp"
 class LoginScreen < Sinatra::Base
     configure do
+        enable :sessions
         set :root, File.dirname(__FILE__)
         set :views, Proc.new { File.join(root, "../templates") }
     end
@@ -10,10 +13,16 @@ class LoginScreen < Sinatra::Base
     end
 
     post '/login' do
+        db_file = "db/users.db"
+        db = SQLite3::Database.new db_file
         # request.body.rewind
         # data = JSON.parse request.body.read
         username = params["username"]
         passwd = params["passwd"]
+
+
+        
+        # redirect '/login'
         "Hello #{params}!"
     end
 end
