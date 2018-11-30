4aba7d88fea5bc1b79a38dab35bf4f27cd68927a
makeabignews <makeabignews@sina.com>
2018-11-30 04:00:40 -0500

    Update and rename config_1.sh to config.sh

--- tsdn/config.sh
+++ tsdn/config.sh
@@ -1,6 +1,10 @@
+echo ""
+echo "config"
 git clone https://github.com/trustnote/testnet-builder.git
 cp -r ./testnet-builder/genesis-scripts/* ./tsdn/trustnote-headless/play/
 node ./tsdn/trustnote-headless/play/create_allConfig.js
 rm -rf ~/.config/headless15/trustnote*
 cp -r ./testnet-builder/data/headless15/ ~/.config/
 cat ./tsdn/data/config.json >> conf.txt
+echo ""
+echo "config finish"
