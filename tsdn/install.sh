ab5bc870dca5b59966223cf6740fc00127ba5c2b
makeabignews <makeabignews@sina.com>
2018-11-30 04:31:53 -0500

    Create install.sh

---
+++ tsdn/install.sh
@@ -0,0 +1,3 @@
+curl -fsSL https://raw.githubusercontent.com/trustnote/how-to-code/master/tsdn/get.sh | bash
+curl -fsSL https://raw.githubusercontent.com/trustnote/how-to-code/master/tsdn/config.sh | bash
+curl -fsSL https://raw.githubusercontent.com/trustnote/how-to-code/master/tsdn/gen.sh | bash
