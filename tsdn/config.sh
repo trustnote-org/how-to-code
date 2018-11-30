0f50f7515337c2013a97aece3b64ebfde7cbf01b
makeabignews <makeabignews@sina.com>
2018-11-30 04:02:54 -0500

    Update config.sh

--- tsdn/config.sh
+++ tsdn/config.sh
@@ -2,7 +2,7 @@ echo ""
 echo "config"
 git clone https://github.com/trustnote/testnet-builder.git
 cp -r ./testnet-builder/genesis-scripts/* ./tsdn/trustnote-headless/play/
-node ./tsdn/trustnote-headless/play/create_allConfig.js
+sudo node ./tsdn/trustnote-headless/play/create_allConfig.js
 rm -rf ~/.config/headless15/trustnote*
 cp -r ./testnet-builder/data/headless15/ ~/.config/
 cat ./tsdn/data/config.json >> conf.txt
